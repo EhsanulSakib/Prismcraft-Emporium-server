fdfe735a8500c2ae1b422a58cdef57129bb9cc89
Kazi Ehsanul Haque <ehsanulsakib.professional@gmail.com>
2024-04-28 19:25:37 +0600

    get (/crafts/:id)

--- index.js
+++ index.js
@@ -24,6 +24,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // crafts database
+    const craftsCollection = client.db("craftsDB").collection('crafts');
+    
+    app.get('/crafts', async(req,res) =>{
+        const cursor = craftsCollection.find();
+        const result = await cursor.toArray();
+  
+        res.send(result)
+    })
+    
+    app.get('/crafts/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollection.findOne(query)
+      res.send(result)
+    })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
